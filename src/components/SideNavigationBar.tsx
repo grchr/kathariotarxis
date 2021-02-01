eda0e408e2eb05c989bf879ec5a7dc639edaf0ff
grchr <grchr@yahoo.com>
2021-02-01 14:27:27 +0200

    Add store provider

--- src/components/SideNavigationBar.tsx
+++ src/components/SideNavigationBar.tsx
@@ -1,43 +1,74 @@
-import React, { useState } from 'react';
-import { Menu, Layout } from 'antd';
-import { HomeOutlined, MailOutlined, AppstoreOutlined, SettingOutlined } from '@ant-design/icons';
-import { NewsIcon } from './MyIcons';
-import { Link } from 'react-router-dom';
-import store from '../store';
+import React, { useState, useEffect } from "react";
+import { Menu, Layout } from "antd";
+import {
+  HomeOutlined,
+  MailOutlined,
+  AppstoreOutlined,
+  SettingOutlined,
+} from "@ant-design/icons";
+import { NewsIcon } from "./MyIcons";
+import { Link } from "react-router-dom";
+import { store, ReduxSelectors } from "../store";
+import { connect } from 'react-redux';
 
 const { Header, Footer, Sider, Content } = Layout;
 
-export function SideNavigationBar(props : any) {
+function SideNavigationBar(props: any) {
+  //   const [active, setActive] = useState(store.getState().status.activePage);
 
-    const[active, setActive] = useState("");
+  const unsubscribe = store.subscribe(() => {
+    console.log("State after dispatch: ", store.getState());
+    setActive(store.getState().status.activePage);
+  });
 
-    const onClickHandler = (e : any) => {
-        console.log(e);
-        setActive(e.key);
-    }
+  const onClickHandler = (e: any) => {
+    props.dispatch({ type: "page/changed", payload: e.key });
+    setActive(e.key);
+  };
 
-    return (
-        <Sider collapsible collapsed={false}  theme={"light"}>
-            <Menu selectedKeys={[active]} onClick={onClickHandler} mode="inline">
-                <Menu.Item key="airman" icon={<HomeOutlined/>}>
-                    Airmen
-                    <Link to="/airmen"></Link>
-                </Menu.Item>
-                <Menu.Item key="chore" icon={<MailOutlined/>}>
-                    Chores
-                    <Link to="/chore"></Link>
-                </Menu.Item>
-                <Menu.Item key="dummy1" icon={<MailOutlined/>}>
-                    Dummy 1
-                </Menu.Item>
-                <Menu.Item key="dummy2" icon={<MailOutlined/>}>
-                    Dummy 2
-                </Menu.Item>
-                <Menu.Item key="dummy3" icon={<MailOutlined/>}>
-                    Dummy 3
-                </Menu.Item>
-            </Menu>
-        </Sider>
-        
-    );
+  const [active, setActive] = useState("");
+
+  // ReduxSelectors.activeNavigationItem$.subscribe((s) => {
+  //     console.log("Inside selector subscription");
+  //     console.log(s);
+  //     setActive(s);
+  // });
+
+  useEffect(() => {
+    setActive(store.getState().status.activePage);
+    console.log(store.getState());
+  }, [store.getState().status.activePage]);
+  return (
+    <Sider collapsible collapsed={false} theme={"light"}>
+      <Menu selectedKeys={[active]} onClick={onClickHandler} mode="inline">
+        <Menu.Item key="airman" icon={<HomeOutlined />}>
+          Airmen
+          <Link to="/airmen"></Link>
+        </Menu.Item>
+        <Menu.Item key="chore" icon={<MailOutlined />}>
+          Chores
+          <Link to="/chore"></Link>
+        </Menu.Item>
+        <Menu.Item key="dummy1" icon={<MailOutlined />}>
+          Dummy 1
+        </Menu.Item>
+        <Menu.Item key="dummy2" icon={<MailOutlined />}>
+          Dummy 2
+        </Menu.Item>
+        <Menu.Item key="dummy3" icon={<MailOutlined />}>
+          Dummy 3{active}
+        </Menu.Item>
+      </Menu>
+    </Sider>
+  );
 }
+
+const mapStateToProps = (state : any) => {
+    console.log('mapStateToProps');
+    console.log(state);
+    return {
+      activePage: state.status.activePage
+    };
+  };
+
+export default connect(mapStateToProps)(SideNavigationBar);
--- src/components/TopNavigationBar.tsx
+++ src/components/TopNavigationBar.tsx
@@ -1,29 +1,61 @@
-import React, { useState } from 'react';
-import { Menu } from 'antd';
-import { HomeOutlined, MailOutlined, AppstoreOutlined, SettingOutlined } from '@ant-design/icons';
-import { NewsIcon } from './MyIcons';
-import { Link } from 'react-router-dom';
-import store from '../store';
+import React, { useState, useEffect } from "react";
+import { Menu } from "antd";
+import {
+  HomeOutlined,
+  MailOutlined,
+  AppstoreOutlined,
+  SettingOutlined,
+} from "@ant-design/icons";
+import { NewsIcon } from "./MyIcons";
+import { Link } from "react-router-dom";
+import { store, ReduxSelectors } from "../store";
+import { connect } from 'react-redux';
 
-export function TopNavigationBar(props : any) {
+function TopNavigationBar(props: any) {
+    console.log('top nav bar props');
+    console.log(props);
+    const [active, setActive] = useState("");
 
-    const[active, setActive] = useState("home");
+    //   ReduxSelectors.activeNavigationItem$.subscribe((s) => {
+    //     console.log("Inside selector subscription");
+    //     console.log(s);
+    //     setActive(s);
+    //   });
 
-    const onClickHandler = (e : any) => {
-        console.log(e);
-        setActive(e.key);
-    }
+    // const unsubscribe = store.subscribe(() => {
+    //     console.log("State after dispatch: ", store.getState());
+    //     setActive(store.getState().status.activePage);
+    // });
+
+    useEffect(() => {
+        setActive(props.activePage);
+    }, [store.getState().status.activePage]);
+
+    const onClickHandler = (e: any) => {
+        props.dispatch({ type: "page/changed", payload: e.key });
+    };
 
     return (
         <Menu selectedKeys={[active]} onClick={onClickHandler} mode="horizontal">
-            <Menu.Item key="home" icon={<HomeOutlined/>}>
-                Home
-                <Link to="/"></Link>
-            </Menu.Item>
-            <Menu.Item key="news" icon={<MailOutlined/>}>
-                News
-                <Link to="/news"></Link>
-            </Menu.Item>
+        <Menu.Item key="home" icon={<HomeOutlined />}>
+            Home
+            <Link to="/"></Link>
+        </Menu.Item>
+        <Menu.Item key="news" icon={<MailOutlined />}>
+            News
+            <Link to="/news"></Link>
+        </Menu.Item>
         </Menu>
     );
 }
+
+
+const mapStateToProps = (state : any) => {
+    console.log('mapStateToProps');
+    console.log(state);
+    return {
+      activePage: state.status.activePage
+    };
+  };
+
+export default connect(mapStateToProps)(TopNavigationBar);
